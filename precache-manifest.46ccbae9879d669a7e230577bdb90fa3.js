840a8e9014e05355dad5f869c94e3ec1c3895737
RicardoERR <57144109+RicardoERR@users.noreply.github.com>
2020-09-11 15:07:32 -0300

    Updates

--- precache-manifest.46ccbae9879d669a7e230577bdb90fa3.js
+++ precache-manifest.46ccbae9879d669a7e230577bdb90fa3.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "5cdc8ada014c6f0d9fbf9ca00f0cc35d",
+    "revision": "80d5642d4de235b7f69c388614a8d24a",
     "url": "/test-calculator-23people/index.html"
   },
   {
-    "revision": "b826e6a008fb454ec533",
+    "revision": "644a4b22d439c5a9d696",
     "url": "/test-calculator-23people/static/css/main.a6b5db06.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/test-calculator-23people/static/js/2.ace1069a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b826e6a008fb454ec533",
-    "url": "/test-calculator-23people/static/js/main.214d8505.chunk.js"
+    "revision": "644a4b22d439c5a9d696",
+    "url": "/test-calculator-23people/static/js/main.f35c4ac6.chunk.js"
   },
   {
     "revision": "486bf0333e14854a55ea",
