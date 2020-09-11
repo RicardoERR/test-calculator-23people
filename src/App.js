cdee0c914089a879cb6da753211e7f27fe754148
RicardoERR <57144109+RicardoERR@users.noreply.github.com>
2020-09-11 10:55:28 -0300

    First commit, initialize app and added librarys

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Test Simple Calculator with ReactJS.</h1>
+      <h4>By Ricardo Riveros Rivera.</h4>
+      <h6>Here will be the Calculator App</h6>
+    </div>
+  );
+}
+
+export default App;
